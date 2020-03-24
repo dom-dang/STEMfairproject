1639f8f7a1ee4380b41d123910092e0b679fbb41
Glitch (hello-webpage) <none>
2020-03-24 17:16:18 +0000

    💽❣️ Checkpoint ./index.html:13238183/810 ./script.js:13238183/702 ./page2.html:13238183/1036 ./page3.html:13238183/2412 ./page-Materials.html:13238183/1042 ./page-Procedure.html:13238183/1057 ./page-Variables.html:13238183/3181 ./page5.html:13238183/1038 ./style.css:13238183/401 ./style2.css:13238183/1374 ./style3.css:13238183/1374 ./style4.css:13238183/2034 ./style5.css:13238183/1373 ./script2.js:13238183/379 ./script3.js:13238183/380 ./script4.js:13238183/380 ./script5.js:13238183/380

--- script3.js
+++ script3.js
@@ -3,3 +3,17 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}

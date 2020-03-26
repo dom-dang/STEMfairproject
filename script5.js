2de914dba66534821ad5c9189172978013f1db06
Glitch (hello-webpage) <none>
2020-03-26 17:59:03 +0000

    🎡🏜 Checkpoint ./script5.js:13238183/542 ./page5.html:13238183/56 ./style5.css:13238183/1875

--- script5.js
+++ script5.js
@@ -18,9 +18,9 @@ window.onclick = function(e) {
   }
 }
 function buttonOne(){
-  document.getElementById("demo").innerHTML = "Today I set up my logbook. I still need tabs and to organize it.";
+  document.getElementById("demo").innerHTML = "9/9/19 Today I set up my logbook. I still need tabs and to organize it.";
 }
 
 function buttonTwo(){
-  document.getElementById("demo").innerHTML = "";
+  document.getElementById("demo").innerHTML = "9/22/19 I worked on my project proposal today because it is due in a week from tomorrow. I managed to finish it in 2 pages which I am proud of because it is short and precise. I am worried if my writing is not professional so I making sure I check and edit my paper over multiple times by multiple people so see if it is presentable. I plan to pass in my propsal early so I can start on other goals like writing out the variables and finalizing my procedure and materials.";
 }

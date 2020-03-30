89426d71fb9b206d5c3d86419a983bfecc2a2ed0
Glitch (hello-webpage) <none>
2020-03-30 15:53:51 +0000

    😀🚡 Checkpoint ./page5.html:13238183/255 ./style5.css:13238183/1415 ./script5.js:13238183/1689

--- script5.js
+++ script5.js
@@ -18,6 +18,27 @@ window.onclick = function(e) {
   }
 }
 
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
 
 var slideIndex=1;
 showSlides(slideIndex);
